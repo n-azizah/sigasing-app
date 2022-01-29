65b2d55681bcb13417a064fa9ec5ac4851371315
n-azizah <azizahn346@gmail.com>
2022-01-30 04:38:28 +0800

    Pemecahan starter.html

---
+++ partials/footer.php
@@ -0,0 +1,10 @@
+ <!-- Main Footer -->
+ <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-block">
+       <b>Version</b> o.1.0
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a href="#">Praktikum Web</a>.</strong> All rights reserved.
+ </footer>
+ <!-- ./wrapper -->
