ed4031ccb50a37813270b919208d7021aba6acf1
n-azizah <azizahn346@gmail.com>
2022-02-17 06:41:18 +0800

    Menampilkan halaman read lokasi

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -32,9 +32,9 @@
                </a>
                <ul class="nav nav-treeview">
                   <li class="nav-item">
-                     <a href="?page=lokasiread" class="nav-link">
+                     <a href="?page=lokasiread " class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
-                        <p>Lokasi</p>
+                        <p>Lokasi </p>
                      </a>
                   </li>
                   <li class="nav-item">
