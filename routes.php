e3561e33c720aae56bcd55d94c74ab929228f55c
n-azizah <azizahn346@gmail.com>
2022-02-17 07:16:34 +0800

    Membuat Halaman Tambah Data

--- routes.php
+++ routes.php
@@ -10,6 +10,9 @@
          file_exists('pages/admin/lokasiread.php') ? include
         'pages/admin/lokasiread.php' : include "pages/404.php";
          break;
+         case 'lokasicreate':
+            file_exists('pages/admin/lokasicreate.php') ? include 'pages/admin/lokasicreate.php' : include "pages/404.php";
+            break;
        default:
        include "pages/404.php";
     }
