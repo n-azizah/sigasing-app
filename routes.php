410d4e9619a1fbe61606de6e39719cd29a287292
n-azizah <azizahn346@gmail.com>
2022-02-26 05:56:49 +0800

    Membuat Halaman Ubah Data

--- routes.php
+++ routes.php
@@ -1,24 +1,23 @@
- <?php
- if (isset($_GET['page'])) {
+<?php
+if (isset($_GET['page'])) {
     $page = $_GET['page'];
     switch ($page) {
-       case '':
-         case 'home':
-       file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
-       break;
-       case 'lokasiread':
-         file_exists('pages/admin/lokasiread.php') ? include
-        'pages/admin/lokasiread.php' : include "pages/404.php";
-         break;
-         case 'lokasicreate':
+        case '':
+        case 'home':
+            file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
+            break;
+        case 'lokasiread':
+            file_exists('pages/admin/lokasiread.php') ? include 'pages/admin/lokasiread.php' : include "pages/404.php";
+            break;
+        case 'lokasicreate':
             file_exists('pages/admin/lokasicreate.php') ? include 'pages/admin/lokasicreate.php' : include "pages/404.php";
             break;
-            case 'lokasiupdate':
-               file_exists('pages/admin/lokasiupdate.php') ? include 'pages/admin/lokasiupdate.php' : include "pages/404.php";
-               break;
-       default:
-       include "pages/404.php";
+        case 'lokasiupdate':
+            file_exists('pages/admin/lokasiupdate.php') ? include 'pages/admin/lokasiupdate.php' : include "pages/404.php";
+            break;
+        default:
+            include "pages/404.php";
     }
- } else {
+} else {
     include "pages/home.php";
- }
+}
